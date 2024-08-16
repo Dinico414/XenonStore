e29981cbe2f305ac19f4d97398c03142a7be0d46
lupluv <lupluv@arcver.se>
2024-08-16 22:46:25 +0200

    dasd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 6
-        versionName = "1.0.2-1"
+        versionName = "1.0.2-2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
