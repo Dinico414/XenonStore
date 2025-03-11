f89b633c4e7d51bd08f05293d49d17a223e8061e
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2025-03-11 07:01:50 +0100

    versions upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.xenon.store"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
