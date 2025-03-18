3e8a37d67757ee54d7d9c413eb574715130a908c
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2025-03-18 13:05:57 +0100

    update code to new switches

--- app/src/main/java/com/xenon/store/MainActivity.kt
+++ app/src/main/java/com/xenon/store/MainActivity.kt
@@ -65,6 +65,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
