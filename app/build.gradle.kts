0534afe97380ef5a656cec249140116c36562658
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2025-03-29 17:34:17 +0100

    Added Landscape Layouts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.xenon.store"
         minSdk = 31
         targetSdk = 35
-        versionCode =9
-        versionName = "1.6.6"
+        versionCode =10
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
