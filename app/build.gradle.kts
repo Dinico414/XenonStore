390b4be8845bc20a10fa86dbc5257a0e4495f44a
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2025-03-31 23:27:04 +0200

    upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.xenon.store"
         minSdk = 31
         targetSdk = 35
-        versionCode =10
-        versionName = "1.7.8"
+        versionCode =11
+        versionName = "1.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
